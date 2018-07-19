5a1a3d38ccbf3c3ff184d5df9da6f5631018e071
zmliang <605593536@qq.com>
2018-07-19 22:55:29 +0800

    rxJava

--- myQuiz/app/src/main/java/com/self/quiz/presenter/GamePresenter.java
+++ myQuiz/app/src/main/java/com/self/quiz/presenter/GamePresenter.java
@@ -1,12 +1,16 @@
 package com.self.quiz.presenter;
 
-import com.self.quiz.game.GameSocketCallBack;
+import android.util.Log;
+
+import com.self.quiz.game.GameSocket;
 import com.self.quiz.game.GameSocketInfo;
 import com.self.quiz.game.IGameStatus;
-
-import org.java_websocket.client.WebSocketClient;
-
 import rx.Observable;
+import rx.Observer;
+import rx.Single;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 
 /**
@@ -16,11 +20,47 @@ import rx.Observable;
  */
 
 public class GamePresenter extends BasePresenter<IGameStatus> {
+    private static final String TAG = GamePresenter.class.getSimpleName();
 
     public void test(){
-        Observable<GameSocketInfo> observable ;
+        Observable.create(new Observable.OnSubscribe<GameSocket>() {
+            // 1. 创建被观察者 & 生产事件
+            @Override
+            public void call(Subscriber<? super GameSocket> subscriber) {
+                    subscriber.onNext(new GameSocket(null));
+                }
+            })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())//观察者在主线程
+                .subscribe(new Observer<GameSocket>() {
+            @Override
+            public void onNext(GameSocket value) {
+                Log.d(TAG, "对Next事件"+ value +"作出响应"  );
+            }
 
+            @Override
+            public void onCompleted() {
 
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "对Error事件作出响应");
+            }
+
+        });
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
